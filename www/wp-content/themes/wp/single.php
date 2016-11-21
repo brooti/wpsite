3f1853da7959a93a1eb09ff754aced76255ee2fe
brooti <procnear@gmail.com>
2016-11-21 20:35:26 +0300

    new

--- www/wp-content/themes/wp/single.php
+++ www/wp-content/themes/wp/single.php
@@ -1,1 +1,34 @@
-single
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+
+	 <div class="content-section">
+        <div class="container">
+            <div class="row">
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				// End the loop.
+				endwhile;
+				?>
+			</div>
+		</div><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
